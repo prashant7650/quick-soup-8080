65072f048cf7d1adf67b73ad63d8f611e241e531
prashant7650 <prashantkad240999@gmail.com>
2023-02-23 21:34:52 +0530

    completed day-3 task

---
+++ Backend/index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const {connection} = require("./db")
+const {userRouter} = require("./routes/user.routes") 
+const {userModel}  =require("./model/user.model")
+
+const cors=require("cors")
+require("dotenv").config()
+const app = express()
+app.use(express.json())
+
+app.get("/",(req,res) => {
+    res.send("Home-Page")
+
+})
+
+app.use("/users",userRouter)
+
+
+app.get("/users",async(req,res) => {
+    let query = req.query
+    console.log(query)
+    try{
+        const users = await userModel.find(query)
+       res.send(users)
+  
+    }catch(err){ 
+        res.send({"msg":"cannot get the User","error":err.message})
+      
+    }
+    
+    
+})
+
+app.listen(process.env.port, async() => {
+    try{
+        await connection
+        console.log("connected to DB")
+    }
+    catch(err){
+        console.log(err)
+        console.log("Can not connected")
+       
+    }
+    console.log(`Port runnig at ${process.env.port}`)
+   
+})
